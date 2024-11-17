a9a84bb71bfdd24c295bf9bf96bbbfaf13fd4006
Shubham Sharma <64421924+Shubhamm097@users.noreply.github.com>
2024-11-16 17:38:11 -0800

    Update GCN.py

--- GCN.py
+++ GCN.py
@@ -13,31 +13,6 @@ import torch.optim as optim
 
 from torch_geometric.nn import GCNConv
 
-import torchvision
-from torchvision.datasets import CIFAR10
-from torchvision import transforms
-
-# import pytorch_lightning as pl
-# from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
-
-# class GCN(nn.Module):
-    
-#     def __init__(self, c_in, c_out):
-#         super().__init__()
-#         self.projection = nn.Linear(c_in, c_out)
-
-#     def forward(self, node_feats, adj_matrix):
-
-#         # node_feats = Tensor with node features of shape[batch_size, num_nodes, c_in]
-#         # adj_matrix = shape: [batch_size, num_nodes, num_nodes]
-#         # num_neighbors = Number of incoming edges
-
-#         num_neighbors = adj_matrix.sum(dim=-1, keepdims=True)
-#         node_feats = self.projection(node_feats)
-#         node_feats = torch.bmm(adj_matrix, node_feats)
-#         node_feats = node_feats / num_neighbors
-#         return node_feats
-
 class GCN(nn.Module):
 
     def __init__(self, hidden_channels, dataset):
